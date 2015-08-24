cb45030211c1e2e53c7f83076f9c69166d8e8c80
Vlad Starodubtsev <v.starodubtsev@ndmsystems.com>
2015-08-24 17:03:18 +0300

    upnphttp: Check for invalid Content-Length header.

--- upnphttp.c
+++ upnphttp.c
@@ -162,6 +162,10 @@ ParseHttpHeaders(struct upnphttp * h)
 				while(*p && (*p < '0' || *p > '9'))
 					p++;
 				h->req_contentlen = atoi(p);
+				if(h->req_contentlen < 0) {
+					DPRINTF(E_WARN, L_HTTP, "Invalid Content-Length %d", h->req_contentlen);
+					h->req_contentlen = 0;
+				}
 			}
 			else if(strncasecmp(line, "SOAPAction", 10)==0)
 			{
